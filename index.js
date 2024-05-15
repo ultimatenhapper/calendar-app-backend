8311d5838290a9a29dbeb338ec0e0c437108004c
Javi <belikemike3057@hotmail.com>
2024-05-15 07:54:41 +0200

    Add some security middleware. Helmet and rate limiting

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require('cors');
 const dotenv = require("dotenv");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const { dbConnection } = require("./db/config");
 
 dotenv.config();
@@ -11,10 +13,25 @@ dbConnection();
 
 //CORS
 app.use(cors())
+
+//Security middleware
+app.use(helmet());
+
+//Rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+app.use(limiter);
+
 //Public directory
 app.use(express.static("public"));
+
 //Read and parse body
 app.use(express.json());
+
 //Routes
 app.use("/api/v1/auth", require("./routes/auth"));
 app.use("/api/v1/events", require("./routes/events"))
